d7692219c1b876f25e897167a07725df3a093721
Juan Perez <jperez4432@gmail.com>
2021-05-19 13:53:58 -0500

    need to convert input to lowercase to match with the array so i can properly get the index of it for the patch options

--- js/movies-project.js
+++ js/movies-project.js
@@ -19,6 +19,7 @@ const getMovies = () => {
                 htmlStr = `${htmlStr}<h1>${movie.title.toUpperCase()}</h1><p>Rating-${movie.rating}/5</p><p>${movie.genre}</p><p id="plot">${movie.plot}</p>`
             }
             $('#container').html(htmlStr)
+
         })
 }
 //USER ADD MOVIES
@@ -30,10 +31,15 @@ const getMovies = () => {
 $("#addMovie").click(() => {
     let userMovie = $('#userMovieName').val();
     let userRating = $('#userMovieRating').val();
+    let userGenre = $('#userMovieGenre').val();
+    let userPlot = $('#userMoviePlot').val();
     let newMovie = {
         "title": userMovie,
-        "rating": userRating
+        "rating": userRating,
+        "genre": userGenre,
+        "plot": userPlot
     };
+
     let postOptions = {
         method: 'POST',
         headers: {
@@ -47,3 +53,73 @@ $("#addMovie").click(() => {
 
 
 getMovies();
+
+
+
+// EDIT MOVIES
+
+$(".edit").click(function (){
+    prompt("hello");
+});
+
+
+
+$("#saveChanges").click(function (){
+    let editedTitle = $("#editTitle").val();
+    let editedRating = $("#editRating").val();
+    let editedGenre = $("#editGenre").val();
+    let editedPlot = $("#editPlot").val();
+
+
+    let editedMovie = {
+        "title": editedTitle,
+        "rating": editedRating,
+        "genre": editedGenre,
+        "plot": editedPlot
+    };
+
+    let patchOptions = {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(editedMovie),
+    }
+
+    $('#movieChoice').change(function () {
+        let dropdownVal = $(this).val();
+        console.log(dropdownVal)
+    });
+
+    let inputVal = $("#movieChoice").val().toLowerCase();
+console.log(inputVal.toLowerCase());
+
+
+    fetch("https://diagnostic-thirsty-ptarmigan.glitch.me/movies", getOptions)
+        .then(resp => resp.json())
+        .then(movies => {
+            console.log(movies);
+// let lowercaseMovies = movies.toLowerCase();
+//  need to define movies and make all to lowercase/uppercase to properly get the index of it so i can plug it into the inputval
+let inputIndex = inputVal.indexOf(inputVal);
+console.log(inputIndex);
+
+    fetch(`https://diagnostic-thirsty-ptarmigan.glitch.me/movies/${inputVal}`, patchOptions).then(getMovies);
+
+    console.log(editedTitle);
+    console.log(editedRating);
+    console.log(editedGenre);
+    console.log(editedPlot);
+
+    // ending brackets for the then(movies =>) call
+});
+
+
+});
+
+
+// $('#movieChoice').change(function () {
+//     let dropdownVal = $(this).val();
+//     console.log(dropdownVal)
+// });
+
